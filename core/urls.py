3cff758d6d76222a94b1e6411327dcfc8812e28a
McDorey@7 <quesiblaq442@gmail.com>
2026-05-12 09:03:06 +0000

    initial commit - Grand Luxe Hotel hompage, rooms, and about pages

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path # type: ignore
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('rooms/', views.rooms, name='rooms'),
+    path('about/', views.about, name='about'),
+]
---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render # type: ignore
+
+def home(request):
+    """Render the home page."""
+    return render(request, 'core/home.html')
+
+def rooms(request):
+  return render(request,'core/rooms.html')
+def about(request):
+  return render(request,'core/about.html')
