e49cd31e16f72411a3e5b86f5c9546aca6d24f97
McDorey@7 <quesiblaq442@gmail.com>
2026-06-05 04:02:07 +0000

    added admin and migration files

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Booking, ContactInquiry
 
-# Register your models here.
+admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+  list_display = ('first_name', 'last_name', 'email', 'phone', 'check_in', 'check_out', 'room_type', 'created_at')
+  list_filter = ('check_in', 'check_out', 'room_type')
+  search_fields = ('first_name', 'last_name', 'email')
+  
+  @admin.register(ContactInquiry)
+  class ContactInqiry(admin.ModelAdmin):
+    list_display = ('name' , 'email' , 'subject' , 'created_at')
+    list_filter = ('created_at')
+    search_fields = ('name', 'email', 'subject')
--- core/models.py
+++ core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+  first_name =models.CharField(max_length=100)
+  last_name = models.CharField(max_length=100)
+  email = models.EmailField()
+  phone = models.CharField(max_length=20)
+  check_in = models.DateField()
+  check_out = models.DateField()
+  guests = models.IntegerField(default=1)
+  room_type = models.CharField(max_length=100)
+  special_requests = models.TextField(max_length=500, blank=True)
+  promo_code = models.CharField(max_length=50, blank=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self):
+    return f"{self.first_name} {self.last_name} - {self.room_type} ({self.check_in} to {self.check_out})"
+  
+  class ContactInquiry(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+      verbose_name_plural = "Contact Inquiries"
+      
+      def __str__(self):
+        return f"{self.name} - {self.subject}"
+      
